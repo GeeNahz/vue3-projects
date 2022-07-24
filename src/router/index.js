bae58995a7bce744f07c4d58f8c0fb438edf4325
Godswill Nasara <godswillnlm@gmail.com>
2022-07-24 01:33:42 +0100

    added nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "@/views/EventList.vue";
-import EventDetails from "@/views/EventDetails.vue";
+import EventLayout from "@/views/event/Layout.vue";
+import EventDetails from "@/views/event/Details.vue";
+import EventRegister from "@/views/event/Register.vue";
+import EventEdit from "@/views/event/Edit.vue";
 import About from "@/views/About.vue";
 
 const routes = [
@@ -11,13 +14,30 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1 }) // accepts this prop (page) and passes the prop to the EventList component
     // Its like a back and forth thing.
     // it accepts page prop from where it is being called 
-    // and passes it to its own component (EventList)
+    // and passes it to its own component (Event)
   },
   {
     path: "/event/:id",
-    name: "EventDetails",
+    name: "EventLayout",
     props: true,
-    component: EventDetails
+    component: EventLayout,
+    children: [ // nested routes
+      {
+        path: "",
+        name: "EventDetails",
+        component: EventDetails
+      },
+      {
+        path: "register",
+        name: "EventRegister",
+        component: EventRegister
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+        component: EventEdit
+      },
+    ]
   },
   {
     path: "/about",
