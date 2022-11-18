dd33c1567041965d6fc0add6129e079f960f5823
GeeNahz <godswillnlm@gmail.com>
2022-11-18 18:31:09 +0100

    FEATURE: create file upload component

---
+++ components/FileUpload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button class="ui primary button">
+      <i class="upload icon"></i>
+      Save
+    </button>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['fileChange'])
+
+const onFileChange = async (e) => {
+  var files = e.target.files || e.dataTransfer.files
+  if (!files.length) return
+
+  const { snapshot, downloadUrl, metadata } = await uploadFile(files[0])
+  emit('fileChange', snapshot, downloadUrl, metadata)
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/base';
+</style>
