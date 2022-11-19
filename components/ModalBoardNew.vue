6c7b1fe1d3f229265400f78f8dcddd41ea9a4601
GeeNahz <godswillnlm@gmail.com>
2022-11-19 02:22:56 +0100

    REFACTOR: adjust to use ModalBoard component

--- components/ModalBoardNew.vue
+++ components/ModalBoardNew.vue
@@ -1,36 +1,7 @@
 <template>
-  <modal @close="$emit('close')">
-    <template #title>
-      create new board
-    </template>
-    <template #body>
-      <form @keyup.enter="submitHandler">
-        <input-field
-          type="text"
-          label="Title"
-          v-model="data.title"
-          placeholder="Board title"
-        />
-        <input-field
-          type="text"
-          label="Description"
-          v-model="data.description"
-          placeholder="Description"
-        />
-        <div class="color__picker">
-          <input
-            id="colour"
-            type="color"
-            label="Color"
-            v-model="data.color"
-            placeholder="Board color"
-          />
-          <label for="colour" style="font-size: small;">Background color</label>
-        </div>
-      </form>
-    </template>
-    <input @click="submitHandler" class="btn" type="submit" value="Submit" />
-  </modal>
+  <div>
+    <modal-board title="create new board" :data="data" @close="$emit('close')" @submit="submitHandler" />
+  </div>
 </template>
 
 <script setup>
@@ -48,18 +19,20 @@ const data = ref({
   title: '',
   description: '',
   color: '#babdb6',
-  // imageUrl: ''
+  imageUrl: {
+    downloadUrl: ''
+  }
 })
 
-const submitHandler = () => {
+const submitHandler = (newData) => {
   let newBoard = {
     id: uuidv4(),
-    title: data.value.title,
-    description: data.value.description,
+    title: newData.title,
+    description: newData.description,
     order: JSON.stringify([]),
-    color: data.value.color,
+    color: newData.color,
     imageUrl: {
-      downloadUrl: ''
+      downloadUrl: newData.imageUrl.downloadUrl
     }
   }
   // boards.value.push(newBoard)
@@ -68,23 +41,4 @@ const submitHandler = () => {
 }
 </script>
 
-<style lang="scss" scoped>
-@import '../assets/styles/base';
-
-.color__picker {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  & input {
-    margin: 0.2rem 0;
-    outline: none;
-    border: none;
-  }
-}
-.btn {
-  width: 100%;
-
-  @include button-fill;
-}
-</style>
+<style lang="scss" scoped></style>
